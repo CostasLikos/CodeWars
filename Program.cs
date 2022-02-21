6e8035d400ced746ed26011a96b10f5449c89ed8
Costas Likos <costaslikos@gmail.com>
2022-02-21 21:00:48 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    public static class Problem
+    {
+        public static string CamelCase(this string str)
+        {
+            Console.WriteLine(str);
+            return str == "" ? "" : string.Join("", str.Trim().Split(" ").Where(word => !string.IsNullOrWhiteSpace(word)).Select(word => char.ToUpper(word[0]) + word.Substring(1)).ToArray());
+        }
+    }
+}
