c142d16ab4a9e0986787206793431b731468c25b
Costas Likos <93220708+CostasLikos@users.noreply.github.com>
2023-02-09 11:14:23 +0200

    Create 8 kyu I love you, a little , a lot, passionately ... not at all.cs

---
+++ 8 kyu I love you, a little , a lot, passionately ... not at all.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Kata
+{
+  public static string HowMuchILoveYou(int nb_petals)
+  {
+    string[] phrases = { "I love you", "a little", "a lot", "passionately", "madly", "not at all" };
+            return phrases[(nb_petals - 1) % 6];
+  }
+}
